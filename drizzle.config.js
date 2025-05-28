8b15fde383fa0a46fcecbcd04cb7cea0e06f8a9b
adparekh <aparekh0110@gmail.com>
2025-05-28 05:13:29 -0400

    Fixed API key

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./configs/schema.js",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_NeO7kW2qmirE@ep-summer-mountain-a4jqbfqs-pooler.us-east-1.aws.neon.tech/ai-short-video-generator?sslmode=require",
+    url: process.env.POSTGRESQL_KEY,
   },
 });
